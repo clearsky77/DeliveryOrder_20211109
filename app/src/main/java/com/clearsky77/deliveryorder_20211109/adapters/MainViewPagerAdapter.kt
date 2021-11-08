474bd5f3d12d415ade7277f4053deb81ee4b56ea
clearsky77 <clearsky3264@gmail.com>
2021-11-09 04:59:35 +0900

    ViewPager 어댑터 추가 및 작업

---
+++ app/src/main/java/com/clearsky77/deliveryorder_20211109/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.clearsky77.deliveryorder_20211109.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.clearsky77.deliveryorder_20211109.fragments.MyProfileFragment
+import com.clearsky77.deliveryorder_20211109.fragments.PizzaStoreListFragment
+
+class MainViewPagerAdapter( fm: FragmentManager) : FragmentPagerAdapter(fm){
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> PizzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
+    }
+}
