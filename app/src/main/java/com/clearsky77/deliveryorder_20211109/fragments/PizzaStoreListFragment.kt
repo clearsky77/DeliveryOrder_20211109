b51e975693eefed5ed5c4188322ff21f3f3c7d38
clearsky77 <clearsky3264@gmail.com>
2021-11-09 04:51:08 +0900

    UI-탭레이아웃, 뷰페이저 배치 / 프래그먼트-피자가게목록 xml, kt 연결

---
+++ app/src/main/java/com/clearsky77/deliveryorder_20211109/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,18 @@
+package com.clearsky77.deliveryorder_20211109.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.clearsky77.deliveryorder_20211109.R
+
+class PizzaStoreListFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
+    }
+}
