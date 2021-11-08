ee17f8a952b80c799125fbd31a2d7218d1be0945
clearsky77 <clearsky3264@gmail.com>
2021-11-09 04:55:24 +0900

    임시 - 내 프로필 프래그먼트 초안 끝

---
+++ app/src/main/java/com/clearsky77/deliveryorder_20211109/fragments/MyProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.clearsky77.deliveryorder_20211109.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.clearsky77.deliveryorder_20211109.R
+
+class MyProfileFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    //        동작 관련 코드 여기에
+
+    }
+}
