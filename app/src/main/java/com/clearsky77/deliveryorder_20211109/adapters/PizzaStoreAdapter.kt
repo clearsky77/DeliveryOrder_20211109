8e4435d1a75ea44f320f3a2f906e037572f7659f
clearsky77 <clearsky3264@gmail.com>
2021-11-10 21:02:46 +0900

    UI에 데이터 배치 / Glide 라이브러리 추가 / 인터넷 권한 허용

--- app/src/main/java/com/clearsky77/deliveryorder_20211109/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211109/adapters/PizzaStoreAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.clearsky77.deliveryorder_20211109.R
 import com.clearsky77.deliveryorder_20211109.datas.StoreData
 import java.util.zip.Inflater
@@ -31,10 +32,9 @@ class PizzaStoreAdapter(
         val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
         val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
 
-
+        txtStoreName.text = txtStoreName.toString()
+        Glide.with(mContext).load(data.logoURL).into(imgLogo)
 
         return row
-
-
     }
 }
