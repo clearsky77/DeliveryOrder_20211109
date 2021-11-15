29d49e25fcbcc88acc8798b20c1af33ebf024051
clearsky77 <clearsky3264@gmail.com>
2021-11-15 20:44:28 +0900

    넘겨받은 가게 데이터를 상세 UI에 표현

--- app/src/main/java/com/clearsky77/deliveryorder_20211109/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211109/ViewStoreDetailActivity.kt
@@ -2,8 +2,16 @@ package com.clearsky77.deliveryorder_20211109
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.clearsky77.deliveryorder_20211109.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import kotlinx.android.synthetic.main.activity_view_store_detail.txtStoreName
+import kotlinx.android.synthetic.main.activity_view_store_detail.imgLogo
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+
+    lateinit var mStoreData : StoreData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
@@ -17,5 +25,10 @@ class ViewStoreDetailActivity : AppCompatActivity() {
     }
 
     private fun setValue() {
+        mStoreData = intent.getSerializableExtra("store") as StoreData
+
+        txtStoreName.text = mStoreData.name
+        txtPhoneNum.text = mStoreData.phoneNum
+        Glide.with(this).load(mStoreData.logoURL).into(imgLogo)
     }
 }
