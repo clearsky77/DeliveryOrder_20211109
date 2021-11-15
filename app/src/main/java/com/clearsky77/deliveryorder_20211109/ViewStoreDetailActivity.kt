1714083e0a21e291b326da2cea4972f61fccaa3a
clearsky77 <clearsky3264@gmail.com>
2021-11-15 21:09:27 +0900

    전화 주문 버튼 -> 통화 권한 체크 / 통화 연결

--- app/src/main/java/com/clearsky77/deliveryorder_20211109/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/clearsky77/deliveryorder_20211109/ViewStoreDetailActivity.kt
@@ -1,9 +1,15 @@
 package com.clearsky77.deliveryorder_20211109
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.clearsky77.deliveryorder_20211109.datas.StoreData
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 import kotlinx.android.synthetic.main.activity_view_store_detail.txtStoreName
 import kotlinx.android.synthetic.main.activity_view_store_detail.imgLogo
@@ -18,10 +24,27 @@ class ViewStoreDetailActivity : AppCompatActivity() {
 
         setupEvents()
         setValue()
-
     }
 
     private fun setupEvents() {
+        btnCall.setOnClickListener {
+            val pl = object : PermissionListener{
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:${mStoreData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@ViewStoreDetailActivity,
+                        "권한 거부되어 전화 불가함", Toast.LENGTH_SHORT).show()
+                }
+            }
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
     }
 
     private fun setValue() {
